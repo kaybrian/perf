46a07208104f86bd2fa5fe30e4fb2be23800bd55
kaybrian <kayonbrian@gmail.com>
2023-05-04 00:48:59 +0200

    added the defualt onboarding page

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from './AppNav/OnboardingScreen'
+import HomeScreen from './Screens/HomeScreen'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+  const [onboarding, setOnboarding] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem("onboarding").then((value) => {
+      if (value == null) {
+        AsyncStorage.setItem("onboarding", "true");
+        setOnboarding(true);
+      } else {
+        setOnboarding(false);
+      }
+    });
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {
+          !onboarding && (
+            <Stack.Screen
+              name="OnboardingScreen"
+              component={OnboardingScreen}
+              options={{
+                headerShown: false,
+                presentation: "fullScreenModal",
+              }}
+            />
+          )
+        }
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+            presentation: "fullScreenModal",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ AppNav/OnboardingScreen.js
@@ -0,0 +1,48 @@
+import { View, Image } from 'react-native'
+import React from 'react'
+import Onboarding from 'react-native-onboarding-swiper';
+import { useNavigation } from '@react-navigation/native';
+
+const OnboardingScreen = () => {
+    const navigation = useNavigation();
+
+    const DotComponent = ({ selected }) => {
+        return (
+            <View classNam={`w-4 h-4 mx-2 items-center p-2 justify-center rounded-full ${selected && "p-2 border border-red-400"}`}>
+                <View className={`w-2 px-2 rounded-full mx-1 h-2 ${selected ? "bg-blue-700" : "bg-blue-200"}`}>
+
+                </View>
+            </View>
+        )
+    }
+
+
+
+    return (
+        <Onboarding
+            onSkip={() => navigation.replace("Home")}
+            onDone={() => navigation.replace("Home")}
+            DotComponent={DotComponent}
+            pages={
+                [
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image
+                            source={require('../assets/virtual.gif')}
+                            resizeMode='contain'
+                            style={{
+                                width: 400,
+                                height: 400,
+                            }}
+                        />,
+                        title: 'Dreams',
+                        subtitle: "Meet your new virtual personal friend! They're here to keep you company and offer support.",
+                    },
+
+                ]}
+        />
+
+    )
+}
+
+export default OnboardingScreen
